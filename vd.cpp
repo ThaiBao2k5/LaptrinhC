79c0d7b0b9695522507cf78f2ceb66881bd89303
unknown <Thai Bao>
2023-11-20 21:05:55 +0700

    vdbai1

---
+++ vd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+float a;
+ cin>>a;
+if (a<=5)
+{
+	cout<<"hoc sinh trung binh";
+} 
+	else if (a<=7)
+{
+	cout<<"hoc sinh kha";
+} 
+	else if (a<=9)
+{ 
+	cout<<"hoc sinh gioi";
+}
+	else if (a<=10)
+{
+	cout<<"hoc sinh xuat sac";	
+}
+	else
+{
+	cout<<"Vui long chi nhap tu 1 den 10";
+}
+return 0;
+	
+}
